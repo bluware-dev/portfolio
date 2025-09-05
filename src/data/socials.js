a0ceafcc60f0fa7bfdc15b20c1d6176d78f34bd8
bluware-dev <bluware.contact@gmail.com>
2025-09-05 11:23:15 -0300

    chore(data): add socials.js and mock portfolio image (socials.js, portfolio.webp)

---
+++ src/data/socials.js
@@ -0,0 +1,22 @@
+export const SOCIALS = [
+	{
+		platform: 'GitHub',
+		url: 'https://github.com/bluware-dev',
+		icon: 'nf-fa-github',
+	},
+	{
+		platform: 'Instagram',
+		url: 'https://www.instagram.com/bluware.dev/',
+		icon: 'nf-fa-instagram',
+	},
+	{
+		platform: 'LinkedIn',
+		url: 'https://www.linkedin.com/in/bluware/',
+		icon: 'nf-dev-linkedin',
+	},
+	{
+		platform: 'Discord',
+		url: 'https://discord.com/users/1345014903890776128',
+		icon: 'nf-fa-discord',
+	},
+];
